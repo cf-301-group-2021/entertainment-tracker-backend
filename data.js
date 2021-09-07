b485142a7e6235da79f604f2dca64a71459b72fd
stefrie <sriehle@gmail.com>
2021-09-07 11:38:34 -0700

    Add getUserLists method stub

---
+++ data.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const ItemModel = require('./item-model.js');
+const Data = { };
+
+Data.getUserLists = async(req,res,errorHandler) => {
+	try {
+
+	} catch(error) { 
+		console.error(error);
+		errorHandler(error); 
+	}
+}
+
+
+
+
+// Data.addAnItem = async(req,res,next) => {
+// 	try {
+// 		const data = req.body;
+// 		const item = new ItemModel(data);
+// 		await item.save();
+// 		res.status(200).json(item);
+// 	} catch(e) { next(e.message); }
+// 	}
+
+// Data.getAllItems = async(req, res) => {
+// 	const data = req.body;
+// 	const items = await ItemModel.find({});
+// 	res.status(200).json(items);
+// }
+
+// Data.getOneItem = async(req, res) => {
+// 	const id = req.params.id;
+// 	const items = await ItemModel.find({_id:id});
+// 	res.status(200).json(items[0]);
+// }
+
+// Data.deleteOne = async(req, res) => {
+// 	const id = req.params.id;
+// 	const items = await ItemModel.deleteOne({_id:id});
+// 	res.status(200).json(items[0]);
+// }
+
+module.exports = Data;
