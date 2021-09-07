d0f017d84fa5db2ab43047bccaf3f529e61dca6e
Joe Ivans <joseph.ivans@gmail.com>
2021-09-07 14:10:01 -0700

    Fix quotes on ShowModal import in data.js
    - Replaced import single quotes with double quotes in data.js.

--- data.js
+++ data.js
@@ -1,29 +1,32 @@
 "use strict";
 
-const ShowModel = require('./models/ShowModel');
+const ShowModel = require("./models/ShowModel");
 const Data = {};
 
-// Data.addAnItem = async(req,res,next) => {
-// 	try {
-// 		const data = req.body;
-// 		const item = new ItemModel(data);
-// 		await item.save();
-// 		res.status(200).json(item);
-// 	} catch(e) { next(e.message); }
-// 	}
+/*
+Data.addAnItem = async(req,res,next) => {
+	try {
+		const data = req.body;
+		const item = new ItemModel(data);
+		await item.save();
+		res.status(200).json(item);
+	} catch(e) { next(e.message); }
+	}
+*/
 
-// Data.getOneItem = async(req, res) => {
-// 	const id = req.params.id;
-// 	const items = await ItemModel.find({_id:id});
-// 	res.status(200).json(items[0]);
-// }
+/*
+Data.getOneItem = async(req, res) => {
+	const id = req.params.id;
+	const items = await ItemModel.find({_id:id});
+	res.status(200).json(items[0]);
+}
+*/
 
 Data.searchTvShows = async (req, res, errorHandler) => {
   try {
 
   } catch (error) {
     console.error(error);
-    errorHandler(error);
   }
 };
 
@@ -79,7 +82,6 @@ Data.getShowUpdates = async (req, res, errorHandler) => {
 
   } catch (error) {
     console.error(error);
-    errorHandler(error);
   }
 };
 
