61c41df02d011663615db04cea72f5fb6f3bf1fb
Joe Ivans <joseph.ivans@gmail.com>
2021-09-07 13:54:25 -0700

    Fix User model userShows data type
    - Replaced ShowModel with Show to match the class export definition.

--- models/UserModel.js
+++ models/UserModel.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   userEmail: { type: "String", required: true }, // unique id
   userPassword: { type: "String", required: true },
-  userShows: { type: "ShowModel[]", required: true },
+  userShows: { type: "Show[]", required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
