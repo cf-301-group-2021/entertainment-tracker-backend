7aae3746e5299298696c1d66133b5dea3b6d4061
Joe Ivans <joseph.ivans@gmail.com>
2021-09-08 03:02:40 -0700

    Add userAuthController
    - Added userAuthController.

---
+++ controllers/userAuthController.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+const HttpStatusCode = require("../common/httpStatusCodes");
+
+async function loginUser(req, res) {
+  try {
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+async function logoutUser(req, res) {
+  try {
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  loginUser,
+  logoutUser,
+};
