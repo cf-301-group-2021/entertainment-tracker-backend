90fd0746d013171805efda191e069722526e59bb
Joe Ivans <joseph.ivans@gmail.com>
2021-09-08 03:00:23 -0700

    Add controllers folder to repo structure
    - Added operationalApiController.

---
+++ controllers/operationalApiController.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const HttpStatusCode = require("../common/httpStatusCodes");
+
+async function getShowUpdates(request, response) {
+  try {
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  getShowUpdates,
+};
