e3f34be7baf0b1c5f23faeb11e2b60537fc60ce4
Joe Ivans <joseph.ivans@gmail.com>
2021-09-08 14:53:56 -0700

    Add simple loginUser implementation
    - Adds implementation to log in the user and return a status code.
    - There is no real legitimate auth at this time.

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -3,9 +3,18 @@ const HttpStatusCode = require("../common/httpStatusCodes");
 
 async function loginUser(req, res) {
   try {
-
+    console.log("Logging in user...");
+    console.log(req.body.data);
+    console.log(req.body.data.email, req.body.data.password);
+    res
+      .status(HttpStatusCode.OK)
+      .send({
+        email: req.body.data.email,
+        password: req.body.data.password,
+      });
   } catch (error) {
     console.error(error);
+    res.status(HttpStatusCode.InternalServerError);
   }
 };
 
